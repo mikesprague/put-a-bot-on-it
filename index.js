b5f2bf703a7e9688fa76766c704dcc16aa9a7224
Michael Sprague <mikesprague@gmail.com>
2021-03-01 08:23:44 -0500

    :recycle: Add await to init calls

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ require('dotenv').config();
 const { initCommands } = require('./lib/commands');
 const { initEasterEggs } = require('./lib/easter-eggs');
 
+const { DISCORD_BOT_TOKEN } = process.env;
+
 const client = new discord.Client();
 
 client.on('ready', () => {
   console.log('🐦💬 Bird Bot is ready');
 });
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+client.login(DISCORD_BOT_TOKEN);
 
 client.on('message', async (msg) => {
-  initEasterEggs(msg);
-  initCommands(msg);
+  await initEasterEggs(msg);
+  await initCommands(msg);
 });
