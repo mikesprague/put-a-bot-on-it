71da0b91029a61e1dc1c301094644089234d9081
Michael Sprague <mikesprague@gmail.com>
2021-03-03 07:47:20 -0500

    :sparkles: Send good morning gif in 8am hour

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const discord = require('discord.js');
 require('dotenv').config();
 
+const { getGoodMorning } = require('./lib/helpers');
 const { initCommands } = require('./lib/commands');
 const { initEasterEggs } = require('./lib/easter-eggs');
 
@@ -9,10 +10,26 @@ const { DISCORD_BOT_TOKEN } = process.env;
 const client = new discord.Client();
 
 client.on('ready', () => {
-  console.log('🐦💬 Bird Bot is ready');
+  console.log('🐦💬 Bird Bot is online');
 });
 
 client.login(DISCORD_BOT_TOKEN);
+
+client.setInterval(async () => {
+  const now = new Date();
+  const hour = now.getHours();
+  let messageSent = false;
+  if (hour === 8 && !messageSent) {
+    const gif = await getGoodMorning();
+    console.log('🐦💬 ', gif);
+    messageSent = true;
+  }
+  console.log('🐦💬 messageSent: ', messageSent);
+  if (hour > 8 && messageSent) {
+    messageSent = false;
+  }
+  console.log('🐦💬 client.setInterval ran');
+}, 600000);
 
 client.on('message', async (msg) => {
   await initEasterEggs(msg);
