fca8c0a86ee4558ed2ee91bfa2780d0d64ff03aa
Michael Sprague <ms388@cornell.edu>
2021-03-12 12:32:09 -0500

    :recycle: Tweak ?kanye

--- commands/kanye.js
+++ commands/kanye.js
@@ -1,5 +1,6 @@
 const {
   getRandomColor,
+  getRandomGifByTerm,
   getRandomNum,
   makeApiCall,
   prepareEmbed,
@@ -10,19 +11,21 @@ const { kanyeApi } = require('../lib/urls');
 
 module.exports = {
   name: 'kanye',
-  description: 'Random Kanye West quote',
+  description: 'Random Kanye West quote from https://kanye.rest',
   args: false,
   async execute(msg, args) {
     // console.log(args);
     const apiUrl = kanyeApi();
     const kanyeData = await makeApiCall(apiUrl);
+    // const kanyeGif = await getRandomGifByTerm('kanye');
     const randomColor = getRandomColor();
     const kanyeEmbed = prepareEmbed({
       command: this.name,
       msg,
       embedColor: randomColor,
       embedDescription: `**${kanyeData.quote}**`,
-      embedThumbnail: kanyeHeads[getRandomNum(kanyeHeads.length)],
+      // embedThumbnail: kanyeHeads[getRandomNum(kanyeHeads.length)],
+      embedImage: kanyeHeads[getRandomNum(kanyeHeads.length)],
     });
     sendContent(msg, kanyeEmbed);
   },
