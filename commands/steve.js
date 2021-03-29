bf48a0bc45e0e9c2ecf5c24c8d89ba46d5eeb2a8
Michael Sprague <mikesprague@gmail.com>
2021-03-29 11:48:30 -0400

    :alembic: Experimenting with ?steve

--- commands/steve.js
+++ commands/steve.js
@@ -8,26 +8,23 @@ const {
 
 module.exports = {
   name: 'steve',
+  aliases: ['steveharvey'],
   args: false,
   description: 'Random Steve Harvey gif',
   async execute(msg, args) {
-    const searchTerm =
-      args.length && args[0].length
-        ? `steve harvey ${args.join(' ').toLowerCase()}`
-        : 'steve harvey';
+    const arg =
+      args.length && args[0].length ? args.join(' ').toLowerCase() : '';
+    const searchTerm = arg.length ? `steve harvey ${arg}` : 'steve harvey';
     const steveGifs = await getGifs({ searchTerm });
     const randomNum = getRandomNum(steveGifs.length);
     const randomColor = getRandomColor();
     const steveEmbed = prepareEmbed({
-      command: `${
-        searchTerm.replace('steve harvey', '').trim().length
-          ? `${this.name} ${searchTerm.replace('steve harvey', '').trim()}`
-          : this.name
-      }`,
+      command: arg.length ? `${this.name} ${arg}` : this.name,
       msg,
-      embedImage: steveGifs[Number(randomNum)].images.downsized.url,
+      embedImage: steveGifs[Number(randomNum)].images.original.url,
       embedColor: randomColor,
     });
-    sendContent(msg, steveEmbed);
+    // sendContent(msg, steveEmbed);
+    msg.edit(steveGifs[Number(randomNum)].images.original.url);
   },
 };
