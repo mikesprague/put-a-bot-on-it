6f3b684b4364bb5e38ae1a18dcaa7285bcec2fa4
Michael Sprague <mikesprague@gmail.com>
2022-01-10 20:04:48 +0000

    :recycle: Refactor joke

---
+++ slash-commands/joke.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const { makeApiCall, sendContent } = require('../lib/helpers');
+const { jokeApi } = require('../lib/urls');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('joke')
+    .setDescription('Gets a random joke from an API'),
+  async execute(interaction) {
+    const apiUrl = jokeApi();
+    const apiData = await makeApiCall(apiUrl);
+    const { type, joke, setup, delivery } = apiData;
+    const dadJokeContent = type === 'single' ? joke : `${setup}\n\n${delivery}`;
+    return sendContent(interaction, dadJokeContent);
+  },
+};
