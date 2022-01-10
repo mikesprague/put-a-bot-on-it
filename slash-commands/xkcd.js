d0cf86cbdc3314644558af40a81fe95e2fd18cf6
Michael Sprague <mikesprague@gmail.com>
2022-01-10 16:14:28 -0500

    :recycle: Refactor xkcd

--- slash-commands/xkcd.js
+++ slash-commands/xkcd.js
@@ -1,3 +1,4 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const {
   getRandomNum,
   makeApiCall,
@@ -7,25 +8,26 @@ const {
 const { xkcdApi } = require('../lib/urls');
 
 module.exports = {
-  name: 'xkcd',
-  description: 'Get curent or random XKCD comic',
-  args: false,
-  async execute(msg, args) {
-    const argAliases = ['current', 'today'];
-    const arg = args[0].trim().toLowerCase();
-    const isCurrent = args.length && argAliases.includes(arg);
+  data: new SlashCommandBuilder()
+    .setName('xkcd')
+    .setDescription('Get curent or random XKCD comic')
+    .addBooleanOption((option) =>
+      option
+        .setName('latest')
+        .setDescription('True for latest/False for random'),
+    ),
+  async execute(interaction) {
+    const isCurrent = interaction.options.getBoolean('latest');
     const randumComicNum = getRandomNum(2430);
     const apiUrl = isCurrent ? xkcdApi() : xkcdApi(randumComicNum);
     const apiData = await makeApiCall(apiUrl);
 
     const xkcdEmbed = prepareEmbed({
-      command: isCurrent ? `${this.name} ${arg}` : this.name,
-      msg,
       embedTtitle: apiData.title,
       embedUrl: `https://xkcd.com/${apiData.num}`,
       embedFooter: apiData.alt,
       embedImage: apiData.img,
     });
-    sendEmbed(msg, xkcdEmbed);
+    sendEmbed(interaction, xkcdEmbed);
   },
 };
