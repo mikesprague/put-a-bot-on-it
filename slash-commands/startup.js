7f7f085aab77657438bc125ab88207cd5655f8af
Michael Sprague <mikesprague@gmail.com>
2022-01-11 20:16:23 -0500

    :sparkles: Add /startup command

---
+++ slash-commands/startup.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const {
+  getRandomColor,
+  getRandomGifByTerm,
+  makeApiCall,
+  prepareEmbed,
+  sendEmbed,
+} = require('../lib/helpers');
+const { thisForThatApi } = require('../lib/urls');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('startup')
+    .setDescription('Random idea for a Startup from an API with a random GIF'),
+  async execute(interaction) {
+    const apiUrl = thisForThatApi();
+    const apiData = await makeApiCall(apiUrl);
+    const topicGif = await getRandomGifByTerm(apiData.that, false);
+    const randomColor = getRandomColor();
+    const startupIdea = `${apiData.this} for ${apiData.that}`;
+    const startupEmbed = prepareEmbed({
+      embedTtitle: startupIdea,
+      embedImage: topicGif,
+      embedColor: randomColor,
+    });
+    return sendEmbed(interaction, startupEmbed);
+  },
+};
