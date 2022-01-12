8b0087c80282288f3a92fed9c7dfa8f89c38d8e6
Michael Sprague <mikesprague@gmail.com>
2022-01-11 19:44:28 -0500

    :recycle: Refactor xkcd

--- slash-commands/xkcd.js
+++ slash-commands/xkcd.js
@@ -11,13 +11,16 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('xkcd')
     .setDescription('Get curent or random XKCD comic')
-    .addBooleanOption((option) =>
+    .addStringOption((option) =>
       option
-        .setName('latest')
-        .setDescription('True for latest/False for random'),
+        .setName('comic')
+        .setDescription('Latest or random')
+        .setRequired(true)
+        .addChoice('latest', 'latest')
+        .addChoice('random', 'random'),
     ),
   async execute(interaction) {
-    const isCurrent = interaction.options.getBoolean('latest');
+    const isCurrent = interaction.options.getString('comic') === 'latest';
     const randumComicNum = getRandomNum(2430);
     const apiUrl = isCurrent ? xkcdApi() : xkcdApi(randumComicNum);
     const apiData = await makeApiCall(apiUrl);
