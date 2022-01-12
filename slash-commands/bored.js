79abe2276e5ae455c3933ff6de7371d748aa1d66
Michael Sprague <mikesprague@gmail.com>
2022-01-11 19:51:36 -0500

    :sparkles: Add /bored command

---
+++ slash-commands/bored.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const { makeApiCall, sendContent } = require('../lib/helpers');
+const { boredApi } = require('../lib/urls');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('bored')
+    .setDescription('Get random activity ideas from an API'),
+  async execute(interaction) {
+    const apiUrl = boredApi();
+    const apiData = await makeApiCall(apiUrl);
+    const boredContent = apiData.activity;
+    console.log(boredContent);
+    return sendContent(interaction, boredContent);
+  },
+};
