41040b85b52fb47d5513c00b1ce1cfc342d0f871
Michael Sprague <mikesprague@gmail.com>
2022-01-19 19:41:16 +0000

    :sparkles: Add /wu-tang command
    Returns random wu-tang sticker from giphy api

---
+++ slash-commands/wutang.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const {
+  getRandomNum,
+  getGifs,
+  prepareEmbed,
+  sendEmbed,
+} = require('../lib/helpers');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('wu-tang')
+    .setDescription('Random Wu-Tang Clan sticker from Giphy'),
+  async execute(interaction) {
+    const searchTerm = 'wu-tang-stickers';
+    const wuTangGifs = await getGifs({ searchTerm, stickerSearch: true });
+    const randomNum = getRandomNum(wuTangGifs.length);
+    const embedImage = wuTangGifs[Number(randomNum)].images.original.url;
+    const wuTangEmbed = prepareEmbed({
+      embedImage,
+      embedFooter: `query: ${searchTerm}`,
+    });
+    sendEmbed(interaction, wuTangEmbed);
+  },
+};
