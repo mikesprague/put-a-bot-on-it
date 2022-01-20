ec7ac25e38d00ddafc68869babcbd4afdfdb4181
Michael Sprague <mikesprague@gmail.com>
2022-01-20 15:45:39 +0000

    :recycle: Alphabetize

--- slash-commands/react.js
+++ slash-commands/react.js
@@ -9,18 +9,18 @@ module.exports = {
         .setName('emoji')
         .setDescription('Animated emoji to react with')
         .setRequired(true)
-        .addChoice('party_wizard', '824974801936056320')
-        .addChoice('dumpster_fire', '824963599759048747')
-        .addChoice('cat_jam', '824963110850265108')
-        .addChoice('unicorn_animated', '801535820062392359')
-        .addChoice('mic_drop', '779346781385392178')
         .addChoice('air_quotes', '756186838212935742')
-        .addChoice('this_is_fine', '756184777052389458')
+        .addChoice('angry_parrot', '756186838426714202')
+        .addChoice('cat_jam', '824963110850265108')
+        .addChoice('cheers', '933710349704757268')
+        .addChoice('dumpster_fire', '824963599759048747')
+        .addChoice('mic_drop', '779346781385392178')
         .addChoice('parrot_shuffle', '755823822153842708')
         .addChoice('party_parrot', '755823821830881342')
+        .addChoice('party_wizard', '824974801936056320')
         .addChoice('ship_it_parrot', '824974802136858624')
-        .addChoice('angry_parrot', '756186838426714202')
-        .addChoice('cheers', '933710349704757268'),
+        .addChoice('this_is_fine', '756184777052389458')
+        .addChoice('unicorn_animated', '801535820062392359'),
     ),
   async execute(interaction) {
     const emojiCode = interaction.options.getString('emoji');
