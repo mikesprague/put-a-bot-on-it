80de7add7962b7094cd17d7bc44e596d610d4f63
Michael Sprague <mikesprague@gmail.com>
2022-01-24 20:21:37 +0000

    :recycle: Improve wu

--- slash-commands/wutang.js
+++ slash-commands/wutang.js
@@ -11,10 +11,12 @@ const {
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('wu-tang')
-    .setDescription('Random Wu-Tang Clan sticker from Giphy'),
+    .setDescription('Random Wu-Tang Clan GIF from Giphy'),
   async execute(interaction) {
     const searchTerm = 'wu-tang';
-    const wuTangGifs = await getGifs({ searchTerm, stickerSearch: true });
+    const wuTangGifs = await getGifs({ searchTerm });
+    // const wuTangStickers = await getGifs({ searchTerm, stickerSearch: true });
+    // const allWuGifs = [...wuTangGifs, ...wuTangStickers];
     const randomNum = getRandomNum(wuTangGifs.length);
     const embedImage = wuTangGifs[Number(randomNum)].images.original.url;
     const wuTangEmbed = prepareEmbed({ embedImage });
