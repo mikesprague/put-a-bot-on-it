a97b87e7765d879d5b089176b806e4c070f626f3
Michael Sprague <mikesprague@gmail.com>
2022-01-25 19:44:16 -0500

    :sparkles: Add query option

--- slash-commands/wutang.js
+++ slash-commands/wutang.js
@@ -11,13 +11,20 @@ const {
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('wu-tang')
-    .setDescription('Random Wu-Tang Clan GIF from Giphy'),
+    .setDescription('Random Wu-Tang Clan GIF from Giphy')
+    .addStringOption((option) =>
+      option.setName('query').setDescription('Enter optional search query'),
+    ),
   async execute(interaction) {
-    const searchTerm = 'wu-tang';
+    const searchQuery = interaction.options.getString('query');
+    const useSearchQuery = Boolean(searchQuery && searchQuery.trim().length);
+    const searchTerm = useSearchQuery ? `wu-tang ${searchQuery}` : 'wu-tang';
     const wuTangGifs = await getGifs({ searchTerm });
     // const wuTangStickers = await getGifs({ searchTerm, stickerSearch: true });
     // const allWuGifs = [...wuTangGifs, ...wuTangStickers];
-    const randomNum = getRandomNum(wuTangGifs.length);
+    const randomNum = useSearchQuery
+      ? Math.max(wuTangGifs.length, 10)
+      : getRandomNum(wuTangGifs.length);
     const embedImage = wuTangGifs[Number(randomNum)].images.original.url;
     const wuTangEmbed = prepareEmbed({ embedImage });
     const wuTangEmoji = getCustomEmojiCode('wutang');
