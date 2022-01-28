9c7e8d86d5dd4c39f2ad9bb16a7a1ed94eacd4ed
Michael Sprague <mikesprague@gmail.com>
2022-01-28 15:29:41 +0000

    :recycle: Improve /ron

--- slash-commands/ron.js
+++ slash-commands/ron.js
@@ -12,31 +12,21 @@ const { ronSwansonApi } = require('../lib/urls');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('ron')
-    .setDescription('Random Ron Swanson quote and gif')
-    .addStringOption((option) =>
-      option
-        .setName('size')
-        .setDescription('The gif size')
-        .setRequired(true)
-        .addChoice('large', 'large')
-        .addChoice('small', 'small'),
-    ),
+    .setDescription('Random Ron Swanson quote from an API with GIF from Giphy'),
   async execute(interaction) {
-    const isLarge = interaction.options.getString('size') === 'large';
     const apiUrl = ronSwansonApi();
+    const searchTerm = 'ron swanson';
     const apiData = await makeApiCall(apiUrl);
-    const allGifs = await getGifs({
-      searchTerm: 'ron swanson',
-      stickerSearch: true,
-    });
-    const randomNum = getRandomNum(14);
+    const ronGifs = await getGifs({ searchTerm });
+    const ronStickers = await getGifs({ searchTerm, stickerSearch: true });
+    const allGifs = [...ronGifs, ...ronStickers];
+    const randomNum = getRandomNum(allGifs.length);
     const randomColor = getRandomColor();
-    const randomSticker = allGifs[Number(randomNum)].images.downsized.url;
+    const randomRon = allGifs[Number(randomNum)].images.downsized.url;
     const embed = prepareEmbed({
       embedColor: randomColor,
       embedDescription: apiData[0],
-      embedImage: isLarge ? randomSticker : '',
-      embedThumbnail: isLarge ? '' : randomSticker,
+      embedImage: randomRon,
     });
     return sendEmbed(interaction, embed);
   },
