29e7bdab328fceba494c6d31d83ffaf1cb2eff51
Michael Sprague <mikesprague@gmail.com>
2022-01-28 15:12:53 +0000

    :recycle: Improve kanye

--- slash-commands/kanye.js
+++ slash-commands/kanye.js
@@ -1,37 +1,42 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const {
+  getGifs,
   getRandomNum,
   getRandomColor,
   makeApiCall,
   prepareEmbed,
   sendEmbed,
 } = require('../lib/helpers');
-const { kanyeHeads } = require('../lib/lists');
 const { kanyeApi } = require('../lib/urls');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('kanye')
-    .setDescription('Random Kanye West quote from https://kanye.rest')
+    .setDescription(
+      'Random Kanye West quote from https://kanye.rest and GIF from Giphy',
+    )
     .addStringOption((option) =>
-      option
-        .setName('size')
-        .setDescription('The gif size')
-        .setRequired(true)
-        .addChoice('large', 'large')
-        .addChoice('small', 'small'),
+      option.setName('query').setDescription('Enter optional search query'),
     ),
   async execute(interaction) {
-    const isLarge = interaction.options.getString('size') === 'large';
+    const arg = interaction.options.getString('query');
+    const useArg = Boolean(arg && arg.trim().length);
+    const searchTerm = useArg ? `kanye ${arg}` : 'kanye';
+    const kanyeGifs = await getGifs({ searchTerm });
+    const kanyeStickers = await getGifs({ searchTerm, stickerSearch: true });
+    const allKanyeGifs = [...kanyeGifs, ...kanyeStickers];
     const apiUrl = kanyeApi();
     const randomColor = getRandomColor();
     const kanyeData = await makeApiCall(apiUrl);
-    const randomKanye = kanyeHeads[getRandomNum(kanyeHeads.length)];
+    const randomNum = useArg
+      ? getRandomNum(Math.min(allKanyeGifs.length, 15))
+      : getRandomNum(allKanyeGifs.length);
+    const randomKanye = allKanyeGifs[randomNum];
     const kanyeEmbed = prepareEmbed({
       embedColor: randomColor,
       embedDescription: kanyeData.quote,
-      embedThumbnail: isLarge ? '' : randomKanye,
-      embedImage: isLarge ? randomKanye : '',
+      embedImage: randomKanye,
+      embedFooter: useArg ? `query: ${arg}` : '',
     });
     return sendEmbed(interaction, kanyeEmbed);
   },
