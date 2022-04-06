bf3a69b69e33e9c97a3d66aa7e1218414825d452
Michael Sprague <mikesprague@gmail.com>
2022-04-06 21:24:01 +0000

    :rotating_light: Fix linter warnings

--- slash-commands/national-day.js
+++ slash-commands/national-day.js
@@ -13,7 +13,7 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('national-day')
     .setDescription(
-      'Random national day from National Day Calendar with possibly related GIF from Giphy API',
+      "Random national day for today's date from National Day Calendar (with possibly related GIF from Giphy API)",
     ),
   async execute(interaction) {
     const apiUrl = nationalDayApi();
@@ -21,22 +21,28 @@ module.exports = {
     const nationalDayData = await makeApiCall(apiUrl);
     const randomNum = getRandomNum(nationalDayData.length);
     const { title, description, link } = nationalDayData[Number(randomNum)];
-    
+
     let searchTermArray = title.split(' ');
     const startTermsToRemove = ['national'];
     if (startTermsToRemove.includes(searchTermArray[0].trim().toLowerCase())) {
       searchTermArray.shift();
     }
     const endTermsToRemove = ['day', 'eve'];
-    if (endTermsToRemove.includes(searchTermArray[searchTermArray.length - 1].trim().toLowerCase())) {
+    if (
+      endTermsToRemove.includes(
+        searchTermArray[searchTermArray.length - 1].trim().toLowerCase(),
+      )
+    ) {
       searchTermArray.pop();
     }
-    const searchTerm = searchTermArray.join(' ').toLowerCase().replace('national', '');
-    
+    const searchTerm = searchTermArray
+      .join(' ')
+      .toLowerCase()
+      .replace('national', '');
+
     const nationalDayGifs = await getGifs({ searchTerm });
     const randomGifNum = getRandomNum(nationalDayGifs.length);
     const randomGif = nationalDayGifs[Number(randomGifNum)].images.original.url;
-    console.log(link.replace('http://', 'https://'));
     const nationalDayEmbed = prepareEmbed({
       embedColor: randomColor,
       embedDescription: `[${title}](${link.replace('http://', 'https://')})`,
--- slash-commands/wordle.js
+++ slash-commands/wordle.js
@@ -1,22 +1,28 @@
 const axios = require('axios').default;
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-const {
-  getRandomNum,
-  getRandomColor,
-  getGifs,
-  prepareEmbed,
-  sendEmbed,
-} = require('../lib/helpers');
+const { getRandomNum, getGifs } = require('../lib/helpers');
 const { wordleSolutionApi } = require('../lib/urls');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('wordle')
-    .setDescription(`Random GIF from Giphy (searches current Wordle solution) - warning, could spoil current game`),
+    .setDescription(
+      `Random GIF from Giphy (searches current Wordle solution) - warning, could spoil current game`,
+    ),
   async execute(interaction) {
     const apiUrl = wordleSolutionApi();
-    const searchTerm = await axios.get(apiUrl).then(response => response.data);
+    const searchTerm = await axios
+      .get(apiUrl)
+      .then((response) => response.data)
+      .catch(async (error) => {
+        const searchTerm = await axios
+          .get(apiUrl)
+          .then((response) => response.data)
+          .catch((error) => console.error(error));
+
+        return searchTerm;
+      });
     const wordleGifs = await getGifs({ searchTerm });
     const randomNum = getRandomNum(wordleGifs.length);
     const embedImage = wordleGifs[Number(randomNum)].images.original.url;
