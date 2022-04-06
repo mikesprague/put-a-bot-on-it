7f1d6c498da56705cbc7cd2ddfa936afc219f97b
Michael Sprague <mikesprague@gmail.com>
2022-04-06 18:28:50 +0000

    :recycle: Change URL

--- slash-commands/wordle.js
+++ slash-commands/wordle.js
@@ -14,7 +14,7 @@ module.exports = {
     .setName('wordle')
     .setDescription(`Random GIF from Giphy (searches current Wordle solution) - warning, could spoil current game`),
   async execute(interaction) {
-    const searchTerm = await axios.get('https://free.birdle.app/api/get-wordle-solution/').then(response => response.data);
+    const searchTerm = await axios.get('https://api.m5ls5e.com/api/get-wordle-solution/').then(response => response.data);
     const wordleGifs = await getGifs({ searchTerm });
     const randomNum = getRandomNum(wordleGifs.length);
     const embedImage = wordleGifs[Number(randomNum)].images.original.url;
