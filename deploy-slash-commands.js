0a739018361eec2ece1233f3bbc634cd06a18fe2
Michael Sprague <mikesprague@gmail.com>
2022-07-19 18:49:28 +0000

    :alien: Update for API changes

--- deploy-slash-commands.js
+++ deploy-slash-commands.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import { REST } from '@discordjs/rest';
-import { Routes } from 'discord-api-types/v9';
+import { Routes } from 'discord-api-types/v10';
 import dotenv from 'dotenv';
 
 dotenv.config();
