e2c6edac02db2251329979171780b1d3678fb653
Michael Sprague <mikesprague@gmail.com>
2022-08-18 00:30:56 +0000

    :sparkles: Add /hall-and-oates

---
+++ slash-commands/hall-and-oates.js
@@ -0,0 +1,21 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { getRandomNum, makeApiCall, sendContent } from '../lib/helpers.js';
+
+const { YOUTUBE_API_KEY } = process.env;
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('hall-and-oates')
+    .setDescription('Random music video from YouTube API'),
+  async execute(interaction) {
+    const apiUrl = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&part=id&maxResults=100&order=relevance&q=hall%20and%20oates&safeSearch=none&type=video&videoEmbeddable=true&key=${YOUTUBE_API_KEY}`;
+    const apiData = await makeApiCall(apiUrl, 'GET', {
+      Accept: 'application/json',
+    });
+    // console.log(apiData);
+    const randomNum = getRandomNum(apiData.items.length);
+    const { videoId } = apiData.items[randomNum].id;
+    const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+    return sendContent(interaction, videoUrl);
+  },
+};
