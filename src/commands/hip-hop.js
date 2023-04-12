bb9dcf81849f7ca81e2d591ff6c16e5e29a72d73
Michael Sprague <mikesprague@gmail.com>
2023-04-12 16:19:53 +0000

    refactor(slash-commands/hip-hop): tweak logic

--- src/commands/hip-hop.js
+++ src/commands/hip-hop.js
@@ -65,8 +65,8 @@ export default {
     // console.log(apiData);
     birdLog(`[hip-hop] ${decodeURIComponent(searchString)}`);
     const randomNum =
-      useArg && apiData.items.length > 3
-        ? 3
+      useArg && apiData.items.length > 5
+        ? 5
         : getRandomNum(apiData.items.length);
     const { videoId } = apiData.items[randomNum].id;
     const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
