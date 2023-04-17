33affb3a9d97514948769591b8b0ce629a856654
Michael Sprague <mikesprague@gmail.com>
2023-04-17 15:29:47 +0000

    refactor(national-day): use ai generated image (instead of gif)
    Signed-off-by: GitHub <noreply@github.com>

--- src/commands/national-day.js
+++ src/commands/national-day.js
@@ -1,7 +1,8 @@
+import { Configuration, OpenAIApi } from 'openai';
 import { SlashCommandBuilder } from 'discord.js';
 
 import {
-  getTenorGifs,
+  // getTenorGifs,
   getRandomNum,
   getRandomColor,
   prepareEmbed,
@@ -10,14 +11,21 @@ import {
 } from '../lib/helpers.js';
 import { initNationalDayData } from '../lib/national-day.js';
 
+const { OPEN_AI_API_KEY } = process.env;
+
 export default {
   data: new SlashCommandBuilder()
     .setName('national-day')
     .setDescription(
-      'Random national day from National Day Calendar (w/ possibly related GIF from Tenor API)',
+      'Random national day from National Day Calendar (w/ possibly related AI generated image)',
     ),
   async execute(interaction) {
     await interaction.deferReply();
+    const configuration = new Configuration({
+      apiKey: OPEN_AI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+
     const randomColor = getRandomColor();
 
     const nationalDayData = await initNationalDayData();
@@ -25,33 +33,43 @@ export default {
     const randomNum = getRandomNum(nationalDayData.length);
     const { title, description, link } = nationalDayData[randomNum];
 
-    let searchTermArray = title.split(' ');
-    const startTermsToRemove = ['national', 'international', 'world'];
-    if (startTermsToRemove.includes(searchTermArray[0].trim().toLowerCase())) {
-      searchTermArray.shift();
-    }
-    const endTermsToRemove = ['day', 'eve'];
-    if (
-      endTermsToRemove.includes(
-        searchTermArray[searchTermArray.length - 1].trim().toLowerCase(),
-      )
-    ) {
-      searchTermArray.pop();
-    }
-    const searchTerm = searchTermArray.join(' ').toLowerCase();
+    // let searchTermArray = title.split(' ');
+    // const startTermsToRemove = ['national', 'international', 'world'];
+    // if (startTermsToRemove.includes(searchTermArray[0].trim().toLowerCase())) {
+    //   searchTermArray.shift();
+    // }
+    // const endTermsToRemove = ['day', 'eve'];
+    // if (
+    //   endTermsToRemove.includes(
+    //     searchTermArray[searchTermArray.length - 1].trim().toLowerCase(),
+    //   )
+    // ) {
+    //   searchTermArray.pop();
+    // }
+    // const searchTerm = searchTermArray.join(' ').toLowerCase();
     // const nationalDayGifs = await getGiphyGifs({ searchTerm });
-    let nationalDayGifs = await getTenorGifs({ searchTerm });
-    if (!nationalDayGifs.length) {
-      nationalDayGifs = await getTenorGifs({ searchTerm: 'swedish chef' });
-    }
-    const randomGifNum = getRandomNum(nationalDayGifs.length);
-    // const randomGif = nationalDayGifs[randomGifNum].images.original.url;
-    const randomGif = nationalDayGifs[randomGifNum].media_formats.gif.url;
+    // let nationalDayGifs = await getTenorGifs({ searchTerm });
+    // if (!nationalDayGifs.length) {
+    //   nationalDayGifs = await getTenorGifs({ searchTerm: 'swedish chef' });
+    // }
+    // const randomGifNum = getRandomNum(nationalDayGifs.length);
+    // // const randomGif = nationalDayGifs[randomGifNum].images.original.url;
+    // const randomGif = nationalDayGifs[randomGifNum].media_formats.gif.url;
+
+    const aiPrompt = `action shot representing ${title.toLowerCase()}, photo, detailed image, no text, no words, no text on image`;
+    const aiResponse = await openai.createImage({
+      prompt: aiPrompt,
+      n: 1,
+      size: '1024x1024',
+      user: interaction.user.id,
+    });
+    const aiImage = aiResponse.data.data[0].url;
+
     const nationalDayEmbed = prepareEmbed({
       embedTitle: title,
       embedColor: randomColor,
       embedDescription: `${description} [Read More](${link})`,
-      embedImage: randomGif,
+      embedImage: aiImage,
       embedUrl: link,
     });
     birdLog(`[/national-day] ${title}`);
