00110bf38a16f76b6ef6f892b87d996000eb5304
Michael Sprague <mikesprague@gmail.com>
2023-12-03 11:17:15 -0500

    feat(catfact): use dall-e generated image

--- src/commands/catfact.js
+++ src/commands/catfact.js
@@ -1,8 +1,11 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { AttachmentBuilder, SlashCommandBuilder } from 'discord.js';
+import { v4 as uuidv4 } from 'uuid';
+import OpenAI from 'openai';
+
 import {
   getCustomEmojiCode,
   getRandomColor,
-  getRandomGifByTerm,
+  // getRandomGifByTerm,
   getRandomNum,
   makeApiCall,
   prepareEmbed,
@@ -10,34 +13,52 @@ import {
 } from '../lib/helpers.js';
 import { catFactsApi } from '../lib/urls.js';
 
+const { OPEN_AI_API_KEY } = process.env;
+
 export default {
   data: new SlashCommandBuilder()
     .setName('catfact')
     .setDescription('Random fact from the Cat Facts API'),
   async execute(interaction) {
     await interaction.deferReply();
+    const openai = new OpenAI({
+      apiKey: OPEN_AI_API_KEY,
+    });
     const apiUrl = catFactsApi();
     const apiData = await makeApiCall(apiUrl, 'GET', {
       Accept: 'application/json',
       'User-Agent': 'BirdBot (Discord.js bot on private server)',
     }).then((response) => response.data);
-    const catGif = await getRandomGifByTerm('cat', false);
+    // const catGif = await getRandomGifByTerm('cat', false);
     const randomNum = getRandomNum(apiData.length);
     const randomColor = getRandomColor();
     const catFact = apiData[randomNum];
+    const response = await openai.images.generate({
+      prompt: `${catFact.fact}, captured in a realistic photograph with natural lighting`,
+      n: 1,
+      size: '1024x1024',
+      model: 'dall-e-3',
+      user: interaction.user.id,
+    });
+    const aiImage = response.data[0].url;
+    const aiImageName = `${uuidv4()}.png`;
+    const embedFile = new AttachmentBuilder(aiImage, { name: aiImageName });
+    const embedImage = `attachment://${aiImageName}`;
     const catFactEmbed = prepareEmbed({
       embedDescription: catFact.fact,
-      embedImage: catGif,
+      embedImage,
       embedColor: randomColor,
     });
     const emojiArray = ['cat_jam', 'party_cat'];
     const catEmoji = getCustomEmojiCode(
-      emojiArray[getRandomNum(emojiArray.length)],
+      emojiArray[getRandomNum(emojiArray.length)]
     );
     return sendEmbed({
       interaction,
       content: catFactEmbed,
+      file: embedFile,
       reaction: catEmoji,
+      deferred: true,
     });
   },
 };
