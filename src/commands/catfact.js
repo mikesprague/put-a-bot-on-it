92d85f89b1de1bebc00c0179a438da373e3bd267
Michael Sprague <mikesprague@gmail.com>
2024-06-05 12:05:15 -0400

    refactor(commands/catfact): update gpt model

--- src/commands/catfact.js
+++ src/commands/catfact.js
@@ -36,7 +36,7 @@ export default {
     const catFact = apiData[randomNum];
     console.log(catFact);
     let imagePrompt = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
+      model: 'gpt-4o',
       messages: [
         {
           role: 'system',
