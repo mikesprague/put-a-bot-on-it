924eaaf10e78499b915225791f5f7683e4b75ac1
Michael Sprague <mikesprague@gmail.com>
2025-01-12 19:47:49 -0500

    refactor(lib/national-day): replace axios with fetch

--- src/lib/national-day.js
+++ src/lib/national-day.js
@@ -1,13 +1,11 @@
-import axios from 'axios';
-
 import { birdLog } from './helpers.js';
 
 export const initNationalDayData = async () => {
   birdLog('[initNationalDayData] Fetching National Day data from API');
-  const apiResults = await axios(
-    'https://mikesprague.github.io/api/national-day/',
+  const apiResults = await fetch(
+    'https://mikesprague.github.io/api/national-day/'
   )
-    .then(async (response) => response.data)
+    .then((response) => response.json())
     .catch((error) => birdLog('[national-day] Error: \n', error));
 
   return apiResults.data;
